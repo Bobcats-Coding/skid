aaa117bc07cc9e301c663a95b64f7653b4ce871f
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2023-04-18 09:00:32 +0100

    [skid] Add hook config to test-environment services
     -  Interactors and Runners configs are having the same configuration
        objects, and both are called services under the testEnvironment
        config.
     -  World object creation is a method of the testEnvironment, and
        therefore it does not need additional type parameters.
     -  Pokedex testEnvironment is moved to a separate file.

--- test/test-environment.test.ts
+++ test/test-environment.test.ts
@@ -1,37 +1,218 @@
-import { createTestEnvironment } from './test-environment'
+import { createTestEnvironment, Runner } from './test-environment'
 import type { Interactor } from './test-environment'
 
 import { EMPTY } from 'rxjs'
 
-test('create test environment', async () => {
-  let isStarted = false
-  const testInteractorCreator = (): Interactor<number> => ({
+test('onBeforeAll hook starts the runners', async () => {
+  const { state: state1, creator: creator1 } = setupFakeRunner()
+  const { state: state2, creator: creator2 } = setupFakeRunner()
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'runner', creator: creator1, hook: 'before-all' },
+    service2: { type: 'runner', creator: creator2, hook: 'before-all' },
+  })
+  await testEnvironment.onBeforeAll()
+  expect(state1.isStarted).toBe(true)
+  expect(state2.isStarted).toBe(true)
+})
+
+test('onBeforeAll hook starts the interactors', async () => {
+  const { state: state1, creator: creator1 } = setupFakeInteractor({ context: 1 })
+  const { state: state2, creator: creator2 } = setupFakeInteractor({ context: 2 })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'interactor', creator: creator1, hook: 'before-all' },
+    service2: { type: 'interactor', creator: creator2, hook: 'before-all' },
+  })
+  await testEnvironment.onBeforeAll()
+  expect(state1.isStarted).toBe(true)
+  expect(state2.isStarted).toBe(true)
+})
+
+test('onAfterAll hook stops the runners', async () => {
+  const { state: state1, creator: creator1 } = setupFakeRunner({ isStarted: true })
+  const { state: state2, creator: creator2 } = setupFakeRunner({ isStarted: true })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'runner', creator: creator1, hook: 'before-all' },
+    service2: { type: 'runner', creator: creator2, hook: 'before-all' },
+  })
+  await testEnvironment.onAfterAll()
+  expect(state1.isStarted).toBe(false)
+  expect(state2.isStarted).toBe(false)
+})
+
+test('onAfterAll hook stops the interactors', async () => {
+  const { state: state1, creator: creator1 } = setupFakeInteractor({ isStarted: true, context: 1 })
+  const { state: state2, creator: creator2 } = setupFakeInteractor({ isStarted: true, context: 2 })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'interactor', creator: creator1, hook: 'before-all' },
+    service2: { type: 'interactor', creator: creator2, hook: 'before-all' },
+  })
+  await testEnvironment.onAfterAll()
+  expect(state1.isStarted).toBe(false)
+  expect(state2.isStarted).toBe(false)
+})
+
+test('onBefore hook starts the contexts', async () => {
+  const { state: state1, creator: creator1 } = setupFakeInteractor({ context: 1 })
+  const { state: state2, creator: creator2 } = setupFakeInteractor({ context: 2 })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'interactor', creator: creator1, hook: 'before-all' },
+    service2: { type: 'interactor', creator: creator2, hook: 'before-all' },
+  })
+  const world = testEnvironment.createWorld()
+  await testEnvironment.onBefore(world)
+  expect(state1.isContextStarted).toBe(true)
+  expect(state2.isContextStarted).toBe(true)
+  expect(world.get('service1')).toBe(1)
+  expect(world.get('service2')).toBe(2)
+})
+
+test('world keys should be typesafe', async () => {
+  const { creator: creator1 } = setupFakeInteractor<1>({ context: 1 })
+  const { creator: creator2 } = setupFakeInteractor<2>({ context: 2 })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'interactor', creator: creator1, hook: 'before-all' },
+    service2: { type: 'interactor', creator: creator2, hook: 'before-all' },
+  })
+  const world = testEnvironment.createWorld()
+  await testEnvironment.onBefore(world)
+  // @ts-expect-error
+  expect(() => world.get('service3')).toThrow()
+  world.get('service1') satisfies 1
+})
+
+test('onBefore hook starts the contexts', async () => {
+  const { state: state1, creator: creator1 } = setupFakeInteractor({ context: 1 })
+  const { state: state2, creator: creator2 } = setupFakeInteractor({ context: 2 })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'interactor', creator: creator1, hook: 'before-all' },
+    service2: { type: 'interactor', creator: creator2, hook: 'before-all' },
+  })
+  const world = testEnvironment.createWorld()
+  await testEnvironment.onBefore(world)
+  expect(state1.isContextStarted).toBe(true)
+  expect(state2.isContextStarted).toBe(true)
+  expect(world.get('service1')).toBe(1)
+  expect(world.get('service2')).toBe(2)
+})
+
+test('onBefore hook starts the "before" services', async () => {
+  const { state: state1, creator: creator1 } = setupFakeInteractor({ context: 1 })
+  const { state: state2, creator: creator2 } = setupFakeInteractor({ context: 2 })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'interactor', creator: creator1, hook: 'before' },
+    service2: { type: 'interactor', creator: creator2, hook: 'before-all' },
+  })
+  const world = testEnvironment.createWorld()
+  await testEnvironment.onBefore(world)
+  expect(state1.isStarted).toBe(true)
+  expect(state2.isStarted).toBe(false)
+})
+
+test('onBeforeAll only hook starts the "before-all" services', async () => {
+  const { state: state1, creator: creator1 } = setupFakeInteractor({ context: 1 })
+  const { state: state2, creator: creator2 } = setupFakeInteractor({ context: 2 })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'interactor', creator: creator1, hook: 'before' },
+    service2: { type: 'interactor', creator: creator2, hook: 'before-all' },
+  })
+  await testEnvironment.onBeforeAll()
+  expect(state1.isStarted).toBe(false)
+  expect(state2.isStarted).toBe(true)
+})
+
+test('onAfter hook stops the "before" services', async () => {
+  const { state: state1, creator: creator1 } = setupFakeInteractor({ isStarted: true, context: 1 })
+  const { state: state2, creator: creator2 } = setupFakeInteractor({ isStarted: true, context: 2 })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'interactor', creator: creator1, hook: 'before' },
+    service2: { type: 'interactor', creator: creator2, hook: 'before-all' },
+  })
+  const world = testEnvironment.createWorld()
+  await testEnvironment.onBefore(world)
+  await testEnvironment.onAfter(world)
+  expect(state1.isStarted).toBe(false)
+  expect(state2.isStarted).toBe(true)
+})
+
+test('onAfterAll only hook stops the "before-all" services', async () => {
+  const { state: state1, creator: creator1 } = setupFakeInteractor({ isStarted: true, context: 1 })
+  const { state: state2, creator: creator2 } = setupFakeInteractor({ isStarted: true, context: 2 })
+  const testEnvironment = createTestEnvironment({
+    service1: { type: 'interactor', creator: creator1, hook: 'before' },
+    service2: { type: 'interactor', creator: creator2, hook: 'before-all' },
+  })
+  await testEnvironment.onAfterAll()
+  expect(state1.isStarted).toBe(true)
+  expect(state2.isStarted).toBe(false)
+})
+
+const setupFakeInteractor = <T = number>({
+  context,
+  isStarted,
+  isContextStarted,
+}: {
+  context: T
+  isStarted?: boolean
+  isContextStarted?: boolean
+}): {
+  state: { isStarted: boolean; isContextStarted: boolean }
+  creator: () => Interactor<T>
+} => {
+  const state = {
+    isStarted: isStarted ?? false,
+    isContextStarted: isContextStarted ?? false,
+  }
+  const creator = (): Interactor<T> => ({
     start: async () => {
-      isStarted = true
+      state.isStarted = true
     },
     stop: async () => {
-      isStarted = false
+      state.isStarted = false
     },
     startContext: async () => {
+      state.isContextStarted = true
       return {
-        context: 1,
+        context,
         reportEntry$: EMPTY,
       }
     },
-    stopContext: async (_: number) => {},
-    onFailure: async (_: number, __: string) => {
+    stopContext: async (_: T) => {
+      state.isContextStarted = false
+    },
+    onFailure: async (context: T, name: string) => {
       return {
-        entry: 'test',
+        entry: `Failure in context ${String(context)} for test ${name}`,
         type: 'text/plain',
       }
     },
   })
-  const testEnvironment = createTestEnvironment({
-    interactors: {
-      testInteractor: testInteractorCreator,
+  return {
+    state,
+    creator,
+  }
+}
+
+const setupFakeRunner = (
+  { isStarted } = {
+    isStarted: false,
+  },
+): {
+  state: { isStarted: boolean }
+  creator: () => Runner
+} => {
+  const state = {
+    isStarted,
+  }
+  const creator = (): Runner => ({
+    start: async () => {
+      state.isStarted = true
     },
-    runners: {},
+    stop: async () => {
+      state.isStarted = false
+    },
   })
-  await testEnvironment.onBeforeAll()
-  expect(isStarted).toBe(true)
-})
+  return {
+    state,
+    creator,
+  }
+}
--- test/test-environment.ts
+++ test/test-environment.ts
@@ -12,22 +12,42 @@ export type Interactor<CONTEXT = any> = {
   onFailure: (context: CONTEXT, testName: string) => Promise<ReportEntry>
 } & Runner
 
-export type TestEnvironmentWorld<INTERACTORS extends Record<string, Interactor>> = {
-  get: (name: keyof INTERACTORS) => GetContext<INTERACTORS[typeof name]>
-  register: (name: keyof INTERACTORS, context: GetContext<INTERACTORS[typeof name]>) => void
+export type NamedInteractor<
+  NAME extends string = string,
+  INTERACTOR extends Interactor = Interactor,
+> = {
+  name: NAME
+  interactor: INTERACTOR
+}
+
+type ServicesToNamedInteractor<
+  SERVICES extends Record<string, InteractorConfig>,
+  KEY = keyof SERVICES,
+> = KEY extends string
+  ? KEY extends keyof SERVICES
+    ? NamedInteractor<KEY, ReturnType<SERVICES[KEY]['creator']>>
+    : never
+  : never
+
+export type TestEnvironmentWorld<INTERACTORS extends NamedInteractor> = {
+  get: <NAME extends INTERACTORS['name']>(
+    name: NAME,
+  ) => INTERACTORS extends NamedInteractor<NAME, infer C> ? GetContext<C> : never
+  register: <NAME extends INTERACTORS['name']>(
+    name: NAME,
+    context: INTERACTORS extends NamedInteractor<NAME, infer I> ? GetContext<I> : never,
+  ) => void
 }
 
 type GetContext<INTERACTOR extends Interactor> = INTERACTOR extends Interactor<infer CONTEXT>
   ? CONTEXT
   : never
 
-export const createWorld = <
-  INTERACTORS extends Record<string, Interactor>,
->(): TestEnvironmentWorld<INTERACTORS> => {
-  const interactors: Map<keyof INTERACTORS, GetContext<INTERACTORS[keyof INTERACTORS]>> = new Map()
+const createWorld = <INTERACTORS extends NamedInteractor>(): TestEnvironmentWorld<INTERACTORS> => {
+  const interactors: Map<INTERACTORS['name'], GetContext<INTERACTORS['interactor']>> = new Map()
 
   return {
-    get: (name: keyof INTERACTORS) => {
+    get: (name) => {
       const interactor = interactors.get(name)
       if (interactor === undefined) {
         throw new Error(`Interactor "${String(name)}" is not registered`)
@@ -50,75 +70,116 @@ export type ReportEntry =
       type: 'image/png'
     }
 
-export type TestEnviornment<INTERACTORS extends Record<string, Interactor>> = {
+export type TestEnviornment<INTERACTORS extends NamedInteractor> = {
   onBeforeAll: () => Promise<void>
   onAfterAll: () => Promise<void>
   onBefore: (world: TestEnvironmentWorld<INTERACTORS>) => Promise<Observable<ReportEntry>>
   onAfter: (world: TestEnvironmentWorld<INTERACTORS>) => Promise<void>
   onFailure: (world: TestEnvironmentWorld<INTERACTORS>, testName: string) => Promise<ReportEntry[]>
+  createWorld: () => TestEnvironmentWorld<INTERACTORS>
+}
+
+type DefaultConfig = { hook?: 'before-all' | 'before' }
+type RunnerConfig = { type: 'runner'; creator: () => Runner } & DefaultConfig
+type InteractorConfig = { type: 'interactor'; creator: () => Interactor } & DefaultConfig
+
+type RunnerInstance = { instance: Runner } & DefaultConfig
+type InteractorInstance = { instance: Interactor } & DefaultConfig
+
+type FilterInteractorConfig<SERVICES extends Record<string, RunnerConfig | InteractorConfig>> = {
+  [K in keyof SERVICES]: SERVICES[K] extends InteractorConfig ? SERVICES[K] : never
 }
 
 export const createTestEnvironment = <
-  INTERACTORS extends Record<string, Interactor>,
-  RUNNERS extends Record<string, Runner>,
->({
-  interactors,
-  runners,
-}: {
-  interactors: Record<keyof INTERACTORS, () => INTERACTORS[keyof INTERACTORS]>
-  runners: Record<keyof RUNNERS, () => RUNNERS[keyof RUNNERS]>
-}): TestEnviornment<INTERACTORS> => {
-  type INTERACTOR = INTERACTORS[keyof INTERACTORS]
-  const interactorsInstances = new Map<keyof INTERACTORS, INTERACTOR>(
-    Object.entries(interactors).map(([name, interactor]) => [name, interactor()]),
+  SERVICES extends Record<string, RunnerConfig | InteractorConfig>,
+>(
+  services: SERVICES,
+): TestEnviornment<ServicesToNamedInteractor<FilterInteractorConfig<SERVICES>>> => {
+  type Interactor = ServicesToNamedInteractor<FilterInteractorConfig<SERVICES>>
+
+  const isInteractorEntry = (
+    entry: [string, RunnerConfig | InteractorConfig],
+  ): entry is [string, InteractorConfig] => entry[1].type === 'interactor'
+
+  const isRunnerEntry = (
+    entry: [string, RunnerConfig | InteractorConfig],
+  ): entry is [string, RunnerConfig] => entry[1].type === 'runner'
+
+  const interactorsInstances = new Map(
+    Object.entries(services)
+      .filter(isInteractorEntry)
+      .map(([key, { creator, hook }]) => [
+        key,
+        { instance: creator(), ...(Boolean(hook) && { hook }) },
+      ]),
   )
+
   const runnersInstances = new Map(
-    Object.entries(runners).map(([name, runner]) => [name, runner()]),
+    Object.entries(services)
+      .filter(isRunnerEntry)
+      .map(([key, { creator, hook }]) => [
+        key,
+        { instance: creator(), ...(Boolean(hook) && { hook }) },
+      ]),
   )
 
-  const asyncMap = async <T, R>(
+  const asyncTransform = async <T, R>(
     iterable: Iterable<T>,
-    mapper: (arg: T) => Promise<R>,
-  ): Promise<R[]> => await Promise.all([...iterable].map(mapper))
+    transform: (list: T[]) => Array<Promise<R>>,
+  ): Promise<R[]> => await Promise.all(transform([...iterable]))
 
-  const forEachRunner = async (mapper: (runner: Runner) => Promise<void>): Promise<void> => {
-    await asyncMap(runnersInstances.values(), mapper)
+  const forEachRunner = async (
+    mapper: (runner: RunnerInstance) => Promise<void>,
+  ): Promise<void> => {
+    await asyncTransform(runnersInstances.values(), (list) => list.map(mapper))
   }
 
-  const forEachInteractor = async (
-    mapper: (interactor: INTERACTOR) => Promise<void>,
+  const forEachBeforeAllInteractor = async (
+    mapper: (interactor: InteractorInstance) => Promise<void>,
   ): Promise<void> => {
-    await asyncMap(interactorsInstances.values(), mapper)
+    await asyncTransform(interactorsInstances.values(), (list) =>
+      list.filter(({ hook }) => hook === 'before-all').map(mapper),
+    )
+  }
+
+  const forEachBeforeInteractor = async (
+    mapper: (interactor: InteractorInstance) => Promise<void>,
+  ): Promise<void> => {
+    await asyncTransform(interactorsInstances.values(), (list) =>
+      list.filter(({ hook }) => hook === 'before').map(mapper),
+    )
   }
 
   const mapInteractors = async <T>(
-    mapper: (key: keyof INTERACTORS, interactor: INTERACTOR) => Promise<T>,
+    mapper: (key: Interactor['name'], interactor: InteractorInstance) => Promise<T>,
   ): Promise<T[]> =>
-    await asyncMap(
-      interactorsInstances.entries(),
-      async ([key, interactor]) => await mapper(key, interactor),
+    await asyncTransform(interactorsInstances.entries(), (list) =>
+      list.map(async ([key, interactor]) => await mapper(key, interactor)),
     )
 
   return {
     onBeforeAll: async () => {
-      await forEachRunner(async (runner) => {
-        await runner.start()
+      await forEachRunner(async ({ instance }) => {
+        await instance.start()
       })
-      await forEachInteractor(async (interactor) => {
-        await interactor.start()
+      await forEachBeforeAllInteractor(async ({ instance }) => {
+        await instance.start()
       })
     },
     onAfterAll: async () => {
-      await forEachRunner(async (runner) => {
-        await runner.stop()
+      await forEachRunner(async ({ instance }) => {
+        await instance.stop()
       })
-      await forEachInteractor(async (interactor) => {
-        await interactor.stop()
+      await forEachBeforeAllInteractor(async ({ instance }) => {
+        await instance.stop()
       })
     },
     onBefore: async (world) => {
-      const reportEntries = await mapInteractors(async (name, interactor) => {
-        const { context, reportEntry$ } = await interactor.startContext()
+      await forEachBeforeInteractor(async ({ instance }) => {
+        await instance.start()
+      })
+      const reportEntries = await mapInteractors(async (name, { instance }) => {
+        const { context, reportEntry$ } = await instance.startContext()
         world.register(name, context)
         return reportEntry$.pipe(
           map(({ entry, type }) =>
@@ -129,14 +190,18 @@ export const createTestEnvironment = <
       return merge<ReportEntry[]>(...reportEntries)
     },
     onAfter: async (world) => {
-      await mapInteractors(async (name, interactor) => {
-        await interactor.stopContext(world.get(name))
+      await mapInteractors(async (name, { instance }) => {
+        await instance.stopContext(world.get(name))
+      })
+      await forEachBeforeInteractor(async ({ instance }) => {
+        await instance.stop()
       })
     },
     onFailure: async (world, testName) => {
-      return await mapInteractors(async (name, interactor) => {
-        return await interactor.onFailure(world.get(name), testName)
+      return await mapInteractors(async (name, { instance }) => {
+        return await instance.onFailure(world.get(name), testName)
       })
     },
+    createWorld: () => createWorld<Interactor>(),
   }
 }
