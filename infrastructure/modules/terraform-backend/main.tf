a74b35b9e15e889f5e9d5d73ea2fe7e46af20999
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2023-06-04 10:27:36 +0100

    [skid] Fix terraform labels

--- infrastructure/modules/terraform-backend/main.tf
+++ infrastructure/modules/terraform-backend/main.tf
@@ -22,15 +22,9 @@ resource "google_storage_bucket" "terraform-backend-bucket" {
 resource "google_kms_key_ring" "terraform-backend-keyring" {
   name = "${var.name}-terraform-backend-keyring"
   location = var.region
-  labels = {
-    "managed-by" = "terraform"
-  }
 }
 
 resource "google_kms_crypto_key" "terraform-backend-crypto-key" {
   name = "${var.name}-terraform-backend-cryptokey"
   key_ring = google_kms_key_ring.terraform-backend-keyring.id
-  labels = {
-    "managed-by" = "terraform"
-  }
 }
