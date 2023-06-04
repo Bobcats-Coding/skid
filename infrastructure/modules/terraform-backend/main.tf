97bb9469f50c54b3e71503f1eaad5331e1d0be43
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2023-06-04 09:54:16 +0100

    [skid] terraform-backend module creates encrypted store

--- infrastructure/modules/terraform-backend/main.tf
+++ infrastructure/modules/terraform-backend/main.tf
@@ -4,7 +4,23 @@ resource "google_storage_bucket" "terraform-backend-bucket" {
   location = var.region
   force_destroy = true
   storage_class = var.storage-class
+
+  uniform_bucket_level_access = true
   versioning {
     enabled = true
   }
+
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.terraform-backend-crypto-key.id
+  }
+}
+
+resource "google_kms_key_ring" "terraform-backend-keyring" {
+  name = "${var.name}-terraform-backend-keyring"
+  location = var.region
+}
+
+resource "google_kms_crypto_key" "terraform-backend-crypto-key" {
+  name = "${var.name}-terraform-backend-cryptokey"
+  key_ring = google_kms_key_ring.terraform-backend-keyring.id
 }
