82b6678edbfbdf366e93f2cd881abd01250f4509
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2023-06-04 10:15:53 +0100

    [skid] Add managed by terraform labels to resources

--- infrastructure/modules/terraform-backend/main.tf
+++ infrastructure/modules/terraform-backend/main.tf
@@ -13,14 +13,24 @@ resource "google_storage_bucket" "terraform-backend-bucket" {
   encryption {
     default_kms_key_name = google_kms_crypto_key.terraform-backend-crypto-key.id
   }
+
+  labels = {
+    "managed-by" = "terraform"
+  }
 }
 
 resource "google_kms_key_ring" "terraform-backend-keyring" {
   name = "${var.name}-terraform-backend-keyring"
   location = var.region
+  labels = {
+    "managed-by" = "terraform"
+  }
 }
 
 resource "google_kms_crypto_key" "terraform-backend-crypto-key" {
   name = "${var.name}-terraform-backend-cryptokey"
   key_ring = google_kms_key_ring.terraform-backend-keyring.id
+  labels = {
+    "managed-by" = "terraform"
+  }
 }
