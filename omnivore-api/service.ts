7b095c11fe95c262736f87af2a38169b8f0df184
Blanka Hooz <hooz.blanka@gmail.com>
2023-08-08 22:46:02 -0700

    [skid] add location and ticket to Omnivore service

---
+++ omnivore-api/service.ts
@@ -0,0 +1,48 @@
+import { createRetrieveLocationRequest, createRetrieveSingleTicketRequest } from './api'
+import type { OmnivoreAPIClient } from './api'
+import { LocationResponse, SingleTicketResponse } from './types'
+
+import type { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+type BaseParams = {
+  apiKey: string
+  locationID: string
+}
+
+type TicketParams = BaseParams & {
+  ticketID: string
+}
+
+type PaymentParams = TicketParams & {
+  paymentID: string
+}
+
+type DiscountParams = TicketParams & {
+  discountID: string
+}
+
+export type OmnivoreService = {
+  retrieveLocation: () => Observable<LocationResponse | undefined>
+  retrieveSingleTicket: (params: TicketParams) => Observable<SingleTicketResponse | undefined>
+}
+
+export const createOmnivoreService = (
+  omnivoreAPIClient: OmnivoreAPIClient,
+  params: BaseParams,
+): OmnivoreService => {
+  const retrieveLocation = (): Observable<LocationResponse> => {
+    return omnivoreAPIClient(createRetrieveLocationRequest(params)).pipe(
+      map(({ location }) => location),
+    )
+  }
+  const retrieveSingleTicket = (params: TicketParams): Observable<SingleTicketResponse> => {
+    return omnivoreAPIClient(createRetrieveSingleTicketRequest(params)).pipe(
+      map(({ ticket }) => ticket),
+    )
+  }
+  return {
+    retrieveLocation,
+    retrieveSingleTicket,
+  }
+}
