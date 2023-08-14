9fb8e5609f4e0ccd87c8e4fe9ac4538b85382319
Blanka Hooz <hooz.blanka@gmail.com>
2023-08-14 13:24:14 -0700

    [skid] add test for Omnivore single ticket endpoint

--- omnivore-api/service.test.ts
+++ omnivore-api/service.test.ts
@@ -1,10 +1,9 @@
 import { createOmnivoreService } from './service'
-import { FAKE_LOCATION, omnivoreAPIClient } from './stub'
-import type { LocationResponse } from './types'
+import { FAKE_LOCATION, FAKE_TICKET, omnivoreAPIClient } from './stub'
+import type { LocationResponse, SingleTicketResponse } from './types'
 
 import { coreMarbles } from '@bobcats-coding/skid/core/marbles'
 
-import type { Observable } from 'rxjs'
 import { filter, map } from 'rxjs/operators'
 
 const omnivoreService = createOmnivoreService(omnivoreAPIClient, {
@@ -25,5 +24,25 @@ test(
     )
     const displayName$ = locations$.pipe(map((location) => location))
     expect(displayName$).toBeObservable('-(n|)', { n: FAKE_LOCATION })
+  }),
+)
+
+test(
+  'retrieve single ticket information',
+  coreMarbles(({ expect }) => {
+    const ticket$ = omnivoreService
+      .retrieveSingleTicket({
+        ticketID: 'ticket123',
+      })
+      .pipe(
+        map((response) => {
+          return response
+        }),
+        filter((tic): tic is SingleTicketResponse => {
+          return tic !== null
+        }),
+      )
+    const displayName$ = ticket$.pipe(map((ticketResponse) => ticketResponse))
+    expect(displayName$).toBeObservable('-(n|)', { n: FAKE_TICKET })
   }),
 )
