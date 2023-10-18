f0351a1ccfc9ed2f7bd791ce9e0b032141fac3bb
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2023-10-18 21:59:28 +0200

    [skid] Add split to utils

--- core/util.test.ts
+++ core/util.test.ts
@@ -1,6 +1,14 @@
-import { makeObjectFromStringLiteral } from './util'
+import { makeObjectFromStringLiteral, split } from './util'
+
+import { assert, type Equals } from 'tsafe'
 
 test('makeObjectFromStringLiteral', () => {
   const obj = makeObjectFromStringLiteral('key' as const, 1)
   expect(obj['key' as const] satisfies number).toBe(1)
+})
+
+test('split is working in a typesafe way', () => {
+  const splitted = split('a.a.a', '.')
+  assert<Equals<typeof splitted, readonly ['a', 'a', 'a']>>()
+  expect(splitted).toEqual(['a', 'a', 'a'])
 })
