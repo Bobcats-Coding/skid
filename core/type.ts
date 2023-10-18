7daec307934163192f4179461d18485f4dbc92ae
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2023-10-18 21:58:33 +0200

    [skid] Add utility types and cover them with unit tests

--- core/type.ts
+++ core/type.ts
@@ -30,6 +30,49 @@ export type GetKey<ENTRIES extends EntryTuple> = ENTRIES[0]
 
 export type GetValue<ENTRIES extends EntryTuple> = ENTRIES[1]
 
+export type FilterRecord2<RECORD extends Record<string | number | symbol, any>, TYPE> = Pick<
+  RECORD,
+  {
+    [K in keyof RECORD]: RECORD[K] extends TYPE ? K : never
+  }[keyof RECORD]
+>
+
 export type FilterRecord<RECORD extends Record<string | number | symbol, any>, TYPE> = {
   [K in keyof RECORD]: RECORD[K] extends TYPE ? RECORD[K] : never
 }
+
+export type Split<STRING extends string, DELIMITER extends string> = SplitHelper<STRING, DELIMITER>
+
+type SplitHelper<
+  STRING extends string,
+  DELIMITER extends string,
+  ACC extends readonly string[] = [],
+> = STRING extends `${infer FIRST}${DELIMITER}${infer REST}`
+  ? SplitHelper<REST, DELIMITER, [...ACC, FIRST]>
+  : readonly [...ACC, STRING]
+
+export type SplitObjectPath<PATH extends string> = Split<PATH, '.'>
+
+export type SplitFilePath<PATH extends string> = Split<PATH, '/'>
+
+export type GetAllPaths<OBJECT, PATH extends string = ''> = PathsHelper<OBJECT, PATH, never>
+
+type PathsHelper<OBJECT, PATH extends string, ACC> =
+  | {
+      [K in keyof OBJECT & string]: PATH extends ''
+        ? K | (OBJECT[K] extends Record<string, any> ? PathsHelper<OBJECT[K], K, K> : never)
+        :
+            | `${PATH}.${K}`
+            | (OBJECT[K] extends Record<string, any>
+                ? PathsHelper<OBJECT[K], `${PATH}.${K}`, `${PATH}.${K}` | ACC>
+                : never)
+    }[keyof OBJECT & string]
+  | ACC
+
+export type GetValueByPath<OBJECT, PATH extends string> = PATH extends keyof OBJECT
+  ? OBJECT[PATH]
+  : PATH extends `${infer K}.${infer Rest}`
+  ? K extends keyof OBJECT
+    ? GetValueByPath<OBJECT[K], Rest & string>
+    : never
+  : never
