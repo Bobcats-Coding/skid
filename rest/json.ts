eb3184d67849bcab6cf68f2e625c48d59870e641
István Kárász <ikarasz@outlook.com>
2023-11-20 13:07:37 +0100

    [bars]: format files

--- rest/json.ts
+++ rest/json.ts
@@ -4,14 +4,16 @@ import { createRestClientCreator } from './service'
 import { type JsonType } from '@bobcats-coding/skid/core/type'
 
 export const jsonFetcher: Fetcher<JsonType | Error> = async (url, request) =>
-  await (await fetch(url, {
-    ...request,
-    headers: {
-      ...request.headers,
-      ...(request.body !== undefined && { 'Content-Type': 'application/json' })
-    },
-    body: request.body !== undefined ? JSON.stringify(request.body) : null,
-  })).json()
+  await (
+    await fetch(url, {
+      ...request,
+      headers: {
+        ...request.headers,
+        ...(request.body !== undefined && { 'Content-Type': 'application/json' }),
+      },
+      body: request.body !== undefined ? JSON.stringify(request.body) : null,
+    })
+  ).json()
 
 export const createJsonRestClient = createRestClientCreator(jsonFetcher)
 
