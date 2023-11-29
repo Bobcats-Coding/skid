a5192ad6bcaae09e8ac9ea0f8a5cb69cadd95c53
István Kárász <ikarasz@outlook.com>
2023-11-29 11:25:17 +0100

    [bars]: rename pre-alpha project

---
+++ rooam-api/fake.ts
@@ -0,0 +1,15 @@
+
+import { of } from 'rxjs';
+import type { RooamService } from './types';
+
+export const createFakeRooamService = (): RooamService => ({
+  getCheckStatus: () => of({
+    status: 'submitted',
+    timestamp: Date.now(),
+    message: 'Ok',
+  }),
+  openCheck: () => of({
+    id: 'fake-id',
+    status: 'accepted'
+  }),
+})
