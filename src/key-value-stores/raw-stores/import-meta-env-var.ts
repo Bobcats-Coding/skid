bc87ca65e5544a2d0b14aeb0bed7d3aa3785a2bf
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2024-02-02 22:34:01 +0000

    [skid] ts module type has to be commonjs because of cucumberjs in sony

--- src/key-value-stores/raw-stores/import-meta-env-var.ts
+++ src/key-value-stores/raw-stores/import-meta-env-var.ts
@@ -5,6 +5,8 @@ export const createImportMetaEnvVarRawKeyValueStore = (): ReadableRawKeyValueSto
 > => {
   return {
     get: (key) => {
+      // eslint-disable-next-line
+      // @ts-ignore - import.meta is not in the types because cucumber needs commonjs
       return import.meta.env[key]
     },
   }
